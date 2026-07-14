900ffc48dd6f4cd6b65c7b068332fbda16f3f6df
semiloreak <semilore.akinbo@gmail.com>
2026-07-14 12:20:13 +0100

    updating App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Discover from './pages/Discover'
-import ContactUs from './pages/ContactUs'
-import AboutUs from './pages/AboutUs'
-import SignUp from './pages/Authentication/SignUp'
-import Login from './pages/Authentication/Login'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Discover from "./pages/Discover";
+import ContactUs from "./pages/ContactUs";
+import AboutUs from "./pages/AboutUs";
+import SignUp from "./pages/Authentication/SignUp";
+import Login from "./pages/Authentication/Login";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element = {<Home />} />
-        <Route path='/discover' element = {<Discover />} />
-        <Route path='/contact-us' element = {<ContactUs />} />
-        <Route path='/about-us' element = {<AboutUs />} />
-        <Route path='/sign-up' element = {<SignUp />} />
-        <Route path='/login' element = {<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
