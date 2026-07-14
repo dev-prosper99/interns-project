ac05f81d07dc9304f4ef65f030553916052c3f72
Tolu-15 <tbabalola@stu.cu.edu.ng>
2026-07-14 10:47:58 +0100

    added footer and intalled font awesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
-import Navbar from './components/layouts/nav'
+import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   )
 }
 
 export default App
+
