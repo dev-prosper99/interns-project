9e3de6a4800ef9bf6bc40c5fe3fbfaa46327dad6
semiloreak <semilore.akinbo@gmail.com>
2026-07-23 15:26:56 +0100

    removed comments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/layouts/Navbar";
 import Footer from "@/components/layouts/Footer";
-// import { CardTextValues } from "@/constants";
-// import { SmallCards } from "@/components/cards";
+import { CardTextValues } from "@/constants";
+import { SmallCards } from "@/components/cards";
 import EventDetail from "./EventDetail";
 import ForgotPassword from "./Authentication/ForgotPassword";
 
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      {/* <div className="bg-[#9EA0A91A] border border-purple-500 flex gap-32">
+      <div className="bg-[#9EA0A91A] border border-purple-500 flex gap-32">
         {CardTextValues.map((values, index) => (
           <SmallCards
             cardNumber={index}
@@ -17,8 +17,8 @@ const Home = () => {
             cardText={values.text}
           />
         ))}
-      </div> */}
-      {/* <EventDetail /> */}
+      </div> *
+      <EventDetail />
       <ForgotPassword />
       <Footer />
     </div>
