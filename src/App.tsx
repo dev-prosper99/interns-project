081b9da8fac4fcb1ecb2a3d2525209dd9fc962e2
Tolu-15 <tbabalola@stu.cu.ed.ng>
2026-09-01 15:59:28 +0100

    Add ProtectedRoute component for route protection in the app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import AboutUs from "./pages/AboutUs";
 import SignUp from "./pages/Authentication/SignUp";
 import Login from "./pages/Authentication/Login";
 import Analytics from "./components/DashBoard/AdminDashboard/Analytics/AnalyticsPage";
+import ProtectedRoute from "./ProtectedRoutes";
 
 
 import Dashboard from "./pages/Dashboard";
@@ -15,17 +16,21 @@ function App() {
   return (
     <div>
       <Routes>
-        
+        <Route element={<ProtectedRoute />}>
+          <Route path="/tickets" element={<TicketsPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/analytics" element={<Analytics />} />
+        </Route>
+
         <Route path="/" element={<Home />} />
-        <Route path="/tickets" element={<TicketsPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        
         <Route path="/discover" element={<Discover />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/analytics" element={<Analytics />} />
+    
       </Routes>
     </div>
   );
